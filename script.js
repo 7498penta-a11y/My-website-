60d8afce81bc544b637a8ea2ab2ec9138bb47b10
7498penta-a11y <7498penta@gmail.com>
2025-09-06 01:45:22 +0900

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// フェードイン＋スクロール連動アニメーション
+// フェードイン＋スクロールアニメーション
 const faders = document.querySelectorAll('.fade');
 const appearOptions = { threshold: 0.2, rootMargin: "0px 0px -50px 0px" };
 const appearOnScroll = new IntersectionObserver((entries, observer) => {
